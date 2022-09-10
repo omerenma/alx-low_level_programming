fc6f5a8dc385e277c6d5e09289857b8fa0904ca8
omerenma <godwin2341@gmail.com>
2022-09-10 00:46:59 -0700

    betty coding style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 	#include <stdio.h>
 	/**
-	 Function entry point - main
-	 Returns 0
+	 * Function entry point - main
+	 * Returns 0
 	 */
 	int main(void)
 	{
-		puts("Programming is like building a multilingual puzzle\n");
+		puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 	}
