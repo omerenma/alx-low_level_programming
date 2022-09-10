b0323e7cfb1a54d83cfe50824bd3e8fa624fb1c1
omerenma <godwin2341@gmail.com>
2022-09-10 01:26:27 -0700

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 	#include <stdio.h>
 	/**
 	 * main -  Entry point
+	 * Description: 'the program prints "Programming is like building a multilingual puzzle"'
 	 *
 	 * Return -  Success returns (0)
 	 */
