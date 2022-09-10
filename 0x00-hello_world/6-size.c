5d7d232573be869cce8afe8c6377c26e855b3aae
omerenma <godwin2341@gmail.com>
2022-09-10 04:34:43 -0700

    6-size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - A C program that prints size of variou types
+ * Return: return 0 (Success)
+ *
+ */ 
+int main(void)
+{
+	printf(sizeof(char));
+	printf(sizeof(int));
+	printf(sizeof(long));
+	printf(sizeof(long long));
+	printf(sizeof(float));
+	return (0);
+}
