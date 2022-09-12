fb1cd3ece672e5c63c6bbfd7a59ec099e8646304
omerenma <godwin2341@gmail.com>
2022-09-12 06:54:19 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: C prgrame to print base16
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num 48;
+
+	while (num <= 102)
+	{
+		putchar(num);
+		if (num == 57)
+			num += 39;
+		++num;
+	}
+	putchar('\n');
+	return (0);
+}
