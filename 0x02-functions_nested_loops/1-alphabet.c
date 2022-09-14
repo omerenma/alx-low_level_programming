16fe2d52e26b002c7fa5abc108c6ddcc6acf8465
omerenma <godwin2341@gmail.com>
2022-09-14 04:38:13 -0700

    fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,19 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * main -Entry point
  * Description: Print alphabet in lowercase
  * Return: Always 0 (Success) 
  */
-int main(void)
+ void print_alphabet(void)
 {
 	char i;
 
 
 	for (i = 'a'; i <= 'z'; i++)
-	{
+
 			_putchar(i);
 		_putchar('\n');
-	}
-	return (0);
+	
+	
 
 }
