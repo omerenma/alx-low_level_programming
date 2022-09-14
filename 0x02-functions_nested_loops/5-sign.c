4455fd2015f319e91193f1f826cc9a53d3ca421f
omerenma <godwin2341@gmail.com>
2022-09-14 15:45:12 -0700

    fix

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,12 +12,12 @@ int print_sign(int n)
 	int result;
 
 
-	if (n > s)
+	if (n > 0)
 	{
 		result = 1;
 		_putchar('+');
 	}
-	else if (n == s)
+	else if (n == 0)
 	{
 		result = 0;
 		_putchar('-');
