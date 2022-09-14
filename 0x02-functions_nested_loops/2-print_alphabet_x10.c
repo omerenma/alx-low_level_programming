de1e7484e3c92181af2ef973a8d06445a7eca454
omerenma <godwin2341@gmail.com>
2022-09-14 06:37:21 -0700

    fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,10 +10,8 @@ void print_alphabet_x10(void)
 	for (j = 0; j <= 9; j++)
 	{
 		for (i = 'a'; i <= 'z'; i++)
-		{
-				_putchar(i);
-			_putchar('\n');
-		}
+			_putchar(i);
+		_putchar('\n');
 	}
 
 }
