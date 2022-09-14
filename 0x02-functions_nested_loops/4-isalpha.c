e303e665b60764f6d7a1ce01d5812d68ec7921a6
omerenma <godwin2341@gmail.com>
2022-09-14 09:06:12 -0700

    is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isalpha - C  program that checks for alphabetic character
+ * @c: integer
+ * Description: The function return 1 if c is a letter, lowercase or uppercase else 0
+ * Return: Returns 1 or 0
+ *
+ *
+ */
+int _isalpha(int c)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == c)
+			return (1);
+		return (0);
+	}
+}
+
