1cbda03f71798b2187ab9a79a4bf5ebc8d88a4a9
omerenma <godwin2341@gmail.com>
2022-09-14 07:06:16 -0700

    fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,8 @@
 #include "main.h"
-/** 
- * print_alphabet_10 - function that prints alphabet 10 times
+/**
+ * main - Entry point
+ * Description: print_alphabet_10 - function that prints alphabet 10 times
+ * Return: Always 0 (Success)
 */
 
 void print_alphabet_x10(void)
