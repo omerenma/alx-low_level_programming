e8c11c9fae571a14180071b28d9295d4d47058f4
omerenma <godwin2341@gmail.com>
2022-09-14 18:16:31 -0700

    fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,12 +12,12 @@ void jack_bauer(void)
 	{
 		for (M = 0; M < 60; M++)
 		{
-			 _putchar((H / 10) + '0');
-			 _putchar((H % 10) + '0');
-			 _putchar(':');
-			 _putchar((M / 10) + '0');
-			 _putchar((M % 10) + '0');
-			 _putchar('\n');
+			_putchar((H / 10) + '0');
+			_putchar((H % 10) + '0');
+			_putchar(':');
+			_putchar((M / 10) + '0');
+			_putchar((M % 10) + '0');
+			_putchar('\n');
 		}
 	}
  }
