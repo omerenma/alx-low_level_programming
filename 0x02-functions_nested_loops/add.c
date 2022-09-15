24b70944d4ea986f3f94ac3942af5ca027be1f34
omerenma <godwin2341@gmail.com>
2022-09-14 17:42:15 -0700

    add

---
+++ 0x02-functions_nested_loops/add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - Add program
+ * @a: integer
+ * @b: integer
+ * Description: C program to add two integers and return resul
+ * Return: a and b
+ *
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
