a7619b3abd5d28e079a782063e0aaee02e689853
omerenma <godwin2341@gmail.com>
2022-09-16 21:54:06 -0700

    fix

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - a function to print
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int i = '0';
+
+	for (; i <= '9'; i++)
+	{
+		_putchat(i);
+	_putchar('\n');
+	}
+}
