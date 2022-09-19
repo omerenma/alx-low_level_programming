b75fd284c5a877007a330664341b5707a56aa9e4
omerenma <godwin2341@gmail.com>
2022-09-19 12:25:17 -0700

    task

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,14 @@
+#include "main.h"
+void puts2(char *str)
+{
+	int len = 0, i = 0;
+
+	while (str[len] != '\0')
+		len++;
+	len -= 1;
+
+	for (; i <= len; i += 2)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
