8ea56cf22c5f154e7ff2c27451b364825dda0ac3
omerenma <godwin2341@gmail.com>
2022-09-19 12:02:09 -0700

    first commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_988  C function with pointer argument
+ * Description: A function that takes a pointer to an int as params and updates the value it points to to 98
+ * @n: An int pointer
+ * Return: Nothing
+ *
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * 1-sawp - Function takes 2 integers and wsaps them
+ * @a: integesr a
+ * @b: integers b
+ * Return: Nothing
+ *
+ */
+void swap_int(int *a, int *b)
+{
+	int swap;
+	
+	*a = *b;
+	*b = aux;
+}
