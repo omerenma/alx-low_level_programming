b6a3539b3b14a7adc924acbc1280b940a80646de
omerenma <godwin2341@gmail.com>
2022-09-26 20:43:49 -0700

    fix

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,8 +14,7 @@ char *_strstr(char *haystack, char *needle)
 	{
 		starth = haystack;
 		needle = startn;
-		
-		while (**haystack == *needle)
+		while (*haystack == *needle)
 		{
 			haystack++;
 			needle++;
