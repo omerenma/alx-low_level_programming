b0d567d56aba63ecedc274086d8b339a208ce5df
omerenma <godwin2341@gmail.com>
2022-09-26 19:58:47 -0700

    fix

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,7 +8,7 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i = 0; max = size * size, sum1 = 0, sum2 = 0;
+	int i = 0, max = size * size, sum1 = 0, sum2 = 0;
 
 	for (; i < max; i += size + 1)
 		sum1 = += a[i];
